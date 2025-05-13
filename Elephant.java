c5ff686da8b1914e96a34a04e2056f5cd0adac17
xxMichaelxx1 <woushinidie@gmail.com>
2025-05-13 08:48:49 -0400

    add the skin, sound and move left right

--- Elephant.java
+++ Elephant.java
@@ -8,8 +8,40 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Elephant extends Actor
 {
+    GreenfootSound elephantSound = new GreenfootSound("elephantcub.mp3");
+    GreenfootImage[] idleRight = new GreenfootImage[8];
+    GreenfootImage[] idleLeft = new GreenfootImage[8];
+    
+    String facing = "right";
+    public Elephant()
+    {
+        for(int i = 0; i < 8; i++)
+        {
+            idleRight[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
+            idleRight[i].scale(100, 100);
+        }
+        
+        for(int i = 0; i < idleLeft.length; i++)
+        {
+            idleLeft[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
+            idleLeft[i].mirrorHorizontally();
+            idleLeft[i].scale(100, 100);
+        }
+        
+
+        setImage(idleRight[0]);
+    }
     public void act()
     {
-        move(1);
+        if(Greenfoot.isKeyDown("left"))
+        {
+            move(-2);
+            facing = "left";
+        }
+        else if(Greenfoot.isKeyDown("right"))
+        {
+            move(2);
+            facing = "right";
+        }
     }
 }
