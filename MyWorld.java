bce145a5852b7103cf85ce68ade158c0e5589672
xxMichaelxx1 <woushinidie@gmail.com>
2025-05-13 09:18:39 -0400

    define score and level

--- MyWorld.java
+++ MyWorld.java
@@ -1,6 +1,9 @@
 import greenfoot.*;
 
 public class MyWorld extends World {
+    public int score = 0;
+    Label scoreLabel;
+    int level = 1;
     public MyWorld() {
         super(600, 400, 1,false);
         
