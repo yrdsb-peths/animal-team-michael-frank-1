877c58e482d495f999ae65a7494fe0c3bf8d1bda
BigFrankk <440044938@gapps.yrdsb.ca>
2025-05-13 08:56:35 -0400

    Update MyWorld.java

--- MyWorld.java
+++ MyWorld.java
@@ -2,9 +2,13 @@ import greenfoot.*;
 
 public class MyWorld extends World {
     public MyWorld() {
-        super(600, 400, 1);
+        super(600, 400, 1,false);
         
         Elephant elephant = new Elephant();
         addObject(elephant, 300, 300);
+        
+        
+        
     }
+    
 }
